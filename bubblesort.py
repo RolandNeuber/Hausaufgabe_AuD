ff6b1b36c1b8c217e096b4ee75609cbfef054927
Roland Neuber <rolandmartinneuber@gmail.com>
2025-01-08 10:43:12 +0100

    Add graphing of Bubblesort runtime fit.

--- bubblesort.py
+++ bubblesort.py
@@ -1,9 +1,18 @@
 from random import randint
-from copy import deepcopy
+from time import perf_counter_ns
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import numpy as np
 
 def Bubblesort(array):
     # Pseudocode
-    # TODO
+    # for i in 0..array_length
+    #    for j in 0..(array_length - i - 1)
+    #        if array[j] > array[j + 1]
+    #            array.swap(j, j+1)
+    #        endif       
+    #    endfor
+    # endfor
     
     for i in range(len(array)):
         for j in range(len(array) - i - 1):
@@ -11,13 +20,42 @@ def Bubblesort(array):
                 array[j + 1], array[j] = array[j], array[j + 1]
     return array
 
-for i in range(1, 11):
-    length = 1000 * i
+def generate_dataset(length):
+    """Generates a random dataset of the specified length and no duplicate elements."""
     dataset = set()
     while len(dataset) != length:
         dataset.add(randint(0, length * 10))
-    dataset = list(dataset)
+    dataset = list(dataset) 
+    return dataset
 
-    expected = deepcopy(dataset)
-    expected.sort()
-    assert(Bubblesort(dataset) == expected)
+def measure_time_ns(function, *args):
+    start = perf_counter_ns()
+    function(*args)
+    end = perf_counter_ns()
+    return end - start
+
+# Laufzeit von Bubblesort wächst Quadratisch O(n²)
+def quadratic_fit(x, a, b, c):
+    return a * x * x + b * x + c
+
+xdata = np.array([i * 1000 for i in range(1, 11)])
+ydata = np.array([])
+
+for i in range(1, 11):
+    length = 1000 * i
+    duration = 0
+    print("Anzahl zu sortierender Elemente:", length)
+    for j in range(10):
+        dataset = generate_dataset(length)
+        duration += measure_time_ns(Bubblesort, dataset)
+    duration /= 10
+    ydata = np.append(ydata, duration / 10e9)
+
+plt.plot(xdata, ydata, label='data')
+popt, pcov = curve_fit(quadratic_fit, xdata, ydata)
+plt.plot(xdata, quadratic_fit(xdata, *popt), label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
+
+plt.xlabel('x in total length of data')
+plt.ylabel('y in seconds')
+plt.legend()
+plt.show()
