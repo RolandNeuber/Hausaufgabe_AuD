342b6204713a7b5e9fd350c5872de271d92f3b22
Roland Neuber <rolandmartinneuber@gmail.com>
2025-02-04 09:05:55 +0100

    Animate simulated annealing.

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from math import sqrt, exp
 from random import random, randint
+from matplotlib import animation
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -28,19 +29,38 @@ def prob_distr(d_E, T):
 
 point_count = 15
 
+fig, ax = plt.subplots()
+ax.set_xlim(0, 40)
+ax.set_ylim(0, 40)
+line, = ax.plot([], [], 'o-', lw=2)
+
 vertices_x = np.array([(random() * 40) for _ in range(point_count)])
 vertices_y = np.array([(random() * 40) for _ in range(point_count)])
 
+current_order = [i for i in range(point_count)]
+all_orders = [deepcopy(current_order)]  # Store all orders for animation
+
+def update(frame):
+    """Update function for animation."""
+    x_vals = vertices_x  # Keep all points
+    y_vals = vertices_y
+    order = all_orders[frame]
+
+    ordered_x = [x_vals[i] for i in order] + [x_vals[order[0]]]
+    ordered_y = [y_vals[i] for i in order] + [y_vals[order[0]]]
+
+    line.set_data(ordered_x, ordered_y)
+    return line,
+
 T_0 = 10
-k_max = 10000
-decay = 0.001
+k_max = 100000
+decay = 0.9999
 
 T = T_0
-current_order = [i for i in range(point_count)]
 best_order = deepcopy(current_order)
 E_0 = target_func(best_order, vertices_x, vertices_y)
 iter_without_change = 0
-needed_iterations = k_max - 1
+needed_iterations = k_max
 
 for k in range(k_max):
     new_order = deepcopy(current_order)
@@ -51,23 +71,21 @@ for k in range(k_max):
         current_order = deepcopy(new_order)
         best_order = deepcopy(new_order)
         E_0 = E_1
+        all_orders.append(deepcopy(current_order))
         iter_without_change = 0
     elif prob_distr(d_E, T) > random():
         current_order = deepcopy(new_order)
         E_0 = E_1
+        all_orders.append(deepcopy(current_order))
     else:
         iter_without_change += 1
     
     if iter_without_change > 500:
         needed_iterations = k
         break
-    T *= (1 - decay)
+    T *= decay
     
 print("needed iterations: " + str(needed_iterations))
 
-ordered_x = [vertices_x[i] for i in best_order] + [vertices_x[best_order[0]]]
-ordered_y = [vertices_y[i] for i in best_order] + [vertices_y[best_order[0]]]
-
-plt.plot(ordered_x, ordered_y, '.')
-plt.plot(ordered_x, ordered_y)
+ani = animation.FuncAnimation(fig, update, frames=len(all_orders), interval=10, blit=True, repeat=False)
 plt.show()
